b46ccc5660c721c4268962c51539a715373809bb
johanverh <johan.verhoeven@outlook.com>
2019-01-19 17:36:36 +0100

    first example

---
+++ example01.py
@@ -0,0 +1,25 @@
+#!/user/bin/python
+import RPi.GPIO as GPIO
+import time
+
+SERVO=5
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO, GPIO.OUT)
+p = GPIO.PWM(SERVO, 50)
+p.start(2.5)
+
+try:
+    while True:
+        p.ChangeDutyCycle(7.5)
+        time.sleep(1)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(1)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(1)
+        print("Done loop")
+
+except KeyboardInterrupt:
+        p.stop()
+        GPIO.cleanup()
+        
+        
